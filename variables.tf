11d441d12b99ed5b1a903343821144f4af6bee84
Troy Perkins <troy.perkins@packetsandflows.cloud>
2023-07-17 15:58:30 -0500

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "AWS_ACCESS_KEY" {
+    type                    = string
+    description             = "Environmental variable for the AWS Access Key"
+}
+
+variable "AWS_SECRET_KEY" {
+    type                    = string
+    description             = "Environmental variable for the AWS Secret Key"
+}
